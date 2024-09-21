21048a39795d8c722a0755a0cdf08eb189ed5f86
Anton Sherbatskiy <89969439+AntonSherbatskiy@users.noreply.github.com>
2024-09-21 23:23:10 +0300

    Revert "Initial commit"

--- lab2/clear.sh
+++
@@ -1,7 +0,0 @@
-source ./variables.sh
-
-rm $base_dockerfile $modified_dockerfile $volume_dockerfile
-docker rm -f $(docker ps -a -q --filter ancestor="${base_image}")
-docker rm -f $(docker ps -a -q --filter ancestor="${modified_image}")
-docker rm -f $(docker ps -a -q --filter ancestor="${volume_image}")
-docker rmi -f $base_image $modified_image $volume_image
